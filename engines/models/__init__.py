89b97e4a08935e1801711b90f9bdc7acd824c1f1
liudawei <david@772311056@qq.com>
2024-02-02 14:38:40 +0800

    empty

--- engines/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time : 2020/11/6 11:29 上午 
-# @Author : lishouxian
-# @Email : lishouxian@cvte.com
-# @File : __init__.py.py 
-# @Software: PyCharm
--- engines/models/text_cnn.py
+++
@@ -1,7 +0,0 @@
-from keras.models import Sequential
-from keras.layers import Embedding, Dense, Conv1D, Dropout
-
-embed1 = Sequential()
-embed1.add(Embedding())
-embed2 = Sequential()
-embed2.add(Embedding())
